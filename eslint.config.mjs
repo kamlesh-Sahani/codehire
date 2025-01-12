e2da7c1e88a935bcb48ab5b1b3a9aa0e526e7902
kamlesh <alex20150001969@gmail.com>
2025-01-12 23:57:07 +0530

    type error solve

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,7 +17,10 @@ const eslintConfig = [
       "rule-name-to-disable": "off", // Replace with the actual rule name
       "another-rule-to-disable": "off",   
       "@typescript-eslint/no-unused-vars": "off", // Disables unused variable checks
-    "@typescript-eslint/no-explicit-any": "off" // Disables explicit any warnings                    
+    "@typescript-eslint/no-explicit-any": "off" ,// Disables explicit any warnings    
+    "@typescript-eslint/no-empty-object-type": "off",  // Disable empty object type warning
+    "prefer-const": "off",
+
     },
   },
 ];
